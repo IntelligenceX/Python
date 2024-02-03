1dbaaeda50375fa1e34c2f4e0857363fd8fe17f7
Christian Heinrich <christian.heinrich@cmlh.id.au>
2024-02-02 23:25:57 -0500

    Remove @IntelligenceX SDK Python examples

--- intelx/examples/download.py
+++
@@ -1,7 +0,0 @@
-from intelxapi import intelx
-
-intelx = intelx()
-search = intelx.search('riseup.net')
-
-# save the first search result file as "file.contents"
-intelx.FILE_READ(search['records'][0]['systemid'], 0, search['records'][0]['bucket'], "file1.bin")
--- intelx/examples/fileview.py
+++
@@ -1,9 +0,0 @@
-from intelxapi import intelx
-
-intelx = intelx()
-result = intelx.search('riseup.net')
-
-# grab file contents of first search result
-contents = intelx.FILE_VIEW(result['records'][0]['type'], result['records'][0]['media'], result['records'][0]['storageid'], result['records'][0]['bucket'])
-
-print(contents)
