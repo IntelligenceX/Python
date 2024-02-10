088b65d4f22b7c9ae5670484fab96a15fd1e8d3a
Christian Heinrich <christian.heinrich@cmlh.id.au>
2024-02-10 16:20:22 -0500

    Add Test of CLI Script
    https://github.com/IntelligenceX/Python/issues/9

---
+++ scripts/tests/test_main.py
@@ -0,0 +1,7 @@
+import pytest
+from intelx import main
+
+def test_main_not_apikey():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        main(["-search", "apple.com"])
+    assert pytest_wrapped_e.value.code == 'No API key specified. Please use the \"-apikey\" parameter or set the environment variable \"INTELX_KEY\".'
